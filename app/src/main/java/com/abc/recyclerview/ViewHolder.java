1f6613fc10b12be29fc66ee4ca77c1f2848ed3c7
ScarlethSalinas <scarlethsg.23@gmail.com>
2022-06-01 18:19:34 -0600

    Actualizacion de viewholder

--- app/src/main/java/com/abc/recyclerview/ViewHolder.java
+++ app/src/main/java/com/abc/recyclerview/ViewHolder.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 //encargado de registrar el enlace al card item
 public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    //hacer esto como tercera parte.
+
     private final List<Source> sourceList;
 
-    //hacer esto como primera parte.
+
     private final Button button1;
     private final Button button2;
     private final ImageView image;
@@ -23,17 +23,17 @@ public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickL
 
     public ViewHolder(@NonNull View itemView, List<Source> sourceList) {
         super(itemView);
-        //hacer esto como primera parte.
+
         button1 = itemView.findViewById(R.id.button1);
         button2 = itemView.findViewById(R.id.button2);
         image = itemView.findViewById(R.id.image);
         title = itemView.findViewById(R.id.title);
 
-        //hacer esto como segunda parte.
+
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
 
-        //hacer esto como tercera parte
+
         this.sourceList = sourceList;
     }
 
@@ -47,11 +47,10 @@ public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickL
 
     @Override
     public void onClick(View v) {
-        //tercera parte
+
         int position = getAdapterPosition();
         Source source = sourceList.get(position);
 
-        //segunda parte
         if (v.getId() == button1.getId()) {
             Toast.makeText(button1.getContext(),
                     "Click on option one, item " + position + " for title " + source.getTitle()
